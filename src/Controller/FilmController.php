c7962b3767c50ada1f111421da073842e8c5443c
khanhchi <le.khanhchi@yahoo.fr>
2023-02-02 12:29:56 +0100

    detail film, delete film

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Film;
+use App\Entity\Genre;
+use App\Entity\Acteur;
 use App\Form\FilmType;
 use App\Repository\FilmRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/film')]
 class FilmController extends AbstractController
@@ -22,14 +25,36 @@ class FilmController extends AbstractController
     }
 
     #[Route('/new', name: 'app_film_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, FilmRepository $filmRepository): Response
+    public function new(Request $request, FilmRepository $filmRepository , EntityManagerInterface $manager): Response
     {
         $film = new Film();
+        // crée formulaire
         $form = $this->createForm(FilmType::class, $film);
+        // récupere le formulaire
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // try catch pour éviter les pieges venant du navigateur (malfaisant)
             try {
+                // récuperer les donnee dans le formulaire
+                $data = $form->getData();
+                
+                // on parcours la liste des acteurs
+                foreach ($data->getActeurs() as $item) {
+                    // instancier puis ajouter les valeur, puis enregistrer dans Entity Film
+                    $acteur = new Acteur();
+                    $acteur->setPrenom($item->getPrenom());
+                    $acteur->setNom($item->getNom());
+                    $film->addActeur($acteur);
+                }
+
+                // on parcours la liste des genre
+                foreach ($data->getGenres() as $item) {
+                    $genre = new Genre();
+                    $genre->setType($item->getType());
+                    $film->addGenre($genre);
+                }
+
                 $filmRepository->save($film, true);
                 return $this->redirectToRoute('app_film_index', [], Response::HTTP_SEE_OTHER);
 
